7688abaef7b8a73a1a88e8d359ca001ba559dacd
mody1993 <modyalzamzamy4@gmail.com>
2026-06-20 01:48:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,50 +7,52 @@ import fetch from 'node-fetch';
 const { WOLF } = wolfjs;
 // ================== ACCOUNTS ==================
 
+
 const ACCOUNTS = [
 {
 email: process.env.U_MAIL_1,
 password: process.env.U_PASS_1,
 allowedPlayers: ['King'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_2,
 password: process.env.U_PASS_2,
 allowedPlayers: ['KSA'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 569,
+targetUserId: 84520028
 },
 {
 email: process.env.U_MAIL_3,
 password: process.env.U_PASS_3,
 allowedPlayers: ['MKH'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 330865,
+targetUserId: 84520024
 },
 {
 email: process.env.U_MAIL_4,
 password: process.env.U_PASS_4,
 allowedPlayers: ['SAA'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 330865,
+targetUserId: 84520024
 },
 {
 email: process.env.U_MAIL_5,
 password: process.env.U_PASS_5,
 allowedPlayers: ['JDH'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 330865,
+targetUserId: 84520024
 },
 {
 email: process.env.U_MAIL_6,
 password: process.env.U_PASS_6,
 allowedPlayers: ['MLK'],
-channelId: 17614046,
-targetUserId: 76023150
+channelId: 330865,
+targetUserId: 84520024
 }
 ];
+
 // ================== CONSTANTS ==================
 
 
